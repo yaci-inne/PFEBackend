5eedfeea5574fea5909e00abcf67a0b7f098e153
yaci-inne <yaci.gaham@gmail.com>
2026-04-28 02:12:38 +0100

    Fix: Ajout de l'import manquant pour health_views

--- backend/urls.py
+++ backend/urls.py
@@ -1,31 +1,19 @@
 """
 URL configuration for store project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from main.views import CustomTokenObtainPairView
 from django.conf import settings
 from django.conf.urls.static import static
+from .health_views import health_check, test_sparql  # 👈 AJOUTEZ CETTE LIGNE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls')),
-    path('api/accessToken/',CustomTokenObtainPairView.as_view(),name='tokenAccess'),
-    path('api/refreshToken/',TokenRefreshView.as_view(),name='tokenRefresh'),
+    path('', include('main.urls')),
+    path('api/accessToken/', CustomTokenObtainPairView.as_view(), name='tokenAccess'),
+    path('api/refreshToken/', TokenRefreshView.as_view(), name='tokenRefresh'),
     path('api/health/', health_check, name='health_check'),
     path('api/sparql/test/', test_sparql, name='test_sparql'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
